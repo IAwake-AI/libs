56452b9bf56bb9cd9a4aa1858eccbad6958365f0
Andrew <andrew@hobbservations.com>
2018-05-15 20:46:17 -0700

    removed submodule

---
+++ facetracker/src/iawake/facetracker/service.py
@@ -0,0 +1,15 @@
+from iawake.facetracker.processor import FaceProcessor
+from iawake.facetracker.processor import RobotProcessor
+
+from iawake.core.contrib.opencv import OpenCVCameraFeed
+from iawake.core.service import SerialService
+
+
+class FaceService(SerialService):
+    feed = OpenCVCameraFeed
+    processor_chain = [FaceProcessor,RobotProcessor]
+
+
+if __name__ == '__main__':
+    for result in FaceService.run(skip_empty=True):
+        print result
